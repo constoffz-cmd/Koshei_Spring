1920d3483f9bfb7222f76e91ffce456076c219b1
Aleksandr Konstantinov <constoff@bk.ru>
2026-01-20 18:37:07 +0500

    no Component

--- src/main/java/koschei/models/Egg6.java
+++ src/main/java/koschei/models/Egg6.java
@@ -1,14 +1,10 @@
 package koschei.models;
 
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.stereotype.Component;
 
-@Component
 public class Egg6 {
 
     private Needle7 needle7;
 
-    @Autowired
     public Egg6(Needle7 needle7) {
         this.needle7 = needle7;
     }
